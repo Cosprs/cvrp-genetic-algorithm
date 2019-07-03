12e6a867a90c3a0a433ffd0d7f4a1def9e2152a1
Maxime Mulder <maxime.mulder@viacesi.fr>
2019-07-03 10:45:11 +0200

    increase algorithm lisibility and execution speed

--- src/genetic.py
+++ src/genetic.py
@@ -1,12 +1,13 @@
+import functools
 import copy
 import random
 
 import utilities
 
-cities = 20
+cities = 100
 capacity = 10
 sample = 50
-generations = 10000
+generations = 500
 
 class Environment:
 
@@ -41,49 +42,80 @@ class Individual:
 	def __init__(self, environment, path):
 		self.environment = environment
 		self.path = path
+		self.score = None
+		self.muted = True
+
 
 	def evaluate(self):
-		score = 0
-		for tour in self.path:
-			score += self.environment.distances[0][tour[0]]
-			i = 0
-			for i in range(1, len(tour)):
-				score += self.environment.distances[tour[i - 1]][tour[i]]
-			a = tour[i]
-			score += self.environment.distances[tour[i]][0]
-		return score
+		if self.muted:
+			self.score = 0
+			for tour in self.path:
+				self.score += self.environment.distances[0][tour[0]]
+				i = 0
+				for i in range(1, len(tour)):
+					self.score += self.environment.distances[tour[i - 1]][tour[i]]
+				a = tour[i]
+				self.score += self.environment.distances[tour[i]][0]
+			self.muted = False
+		return self.score
+
 
 	def mutate(self):
+		self.muted = True
 		j = random.randint(0, self.environment.quantity - 2)
-		for i in range(len(self.path)):
-			j -= len(self.path[i])
+		for i, tour in enumerate(self.path):
+			j -= len(tour)
 			if j < 0:
 				break
 		j = len(self.path[i]) + j
-		m = random.randint(-1, len(self.path) - 1)
-		if m == -1:
-			self.path.append([self.path[i][j]])
-			del self.path[i][j]
-			if len(self.path[i]) == 0:
-				del self.path[i]
+
+		if random.randint(0, 8) == 0:
+			self.append(i, j)
+			self.pop(i, j)
 		else:
+			m = random.randint(0, len(self.path) - 1)
 			n = random.randint(0, self.environment.capacity - 1)
 			if n < len(self.path[m]):
-				self.path[m][n], self.path[i][j] = self.path[i][j], self.path[m][n]
+				self.swap(i, j, m, n)
 			else:
 				if m != i:
 					n = random.randint(0, len(self.path[m]))
-					self.path[m].insert(n, self.path[i][j])
-					del self.path[i][j]
-					if len(self.path[i]) == 0:
-						del self.path[i]
+					self.insert(i, j, m, n)
+					self.pop(i, j)
 				else:
 					n = random.randint(0, len(self.path[m]) - 1)
-					self.path[m][n], self.path[i][j] = self.path[i][j], self.path[m][n]
+					self.swap(i, j, m, n)
+
+
+	def append(self, i, j):
+		self.path.append([self.path[i][j]])
+
+
+	def pop(self, i, j):
+		del self.path[i][j]
+		if len(self.path[i]) == 0:
+			del self.path[i]
+
+
+	def insert(self, i, j, m, n):
+		self.path[m].insert(n, self.path[i][j])
+
+
+	def swap(self, i, j, m, n):
+		self.path[m][n], self.path[i][j] = self.path[i][j], self.path[m][n]
+
 
 coordinates = utilities.get_coordinates(cities, -1000, 1000)
 
 environment = Environment(utilities.coordinates_to_distances(coordinates), capacity)
 
-for individual in environment.individuals:
-	print(str(individual.evaluate()) + ': ' + str(individual.path))
+environment.individuals = sorted(environment.individuals, key=lambda individual: individual.evaluate())
+
+total = 0
+
+for i, individual in enumerate(environment.individuals):
+	score = individual.evaluate()
+	total += score
+	print(str(i) + ': ' + str(score))
+
+print('Average: ' + str(total / len(environment.individuals)))
